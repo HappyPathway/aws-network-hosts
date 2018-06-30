e8c006f247b3bb6f61f54b6eb86eaa2ee9cf69cb
Dave Arnold <darnold@hashicorp.com>
2018-06-30 10:10:06 -0700

    adding dynamic aws credentials

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ variable "count" {
   default = 1
 }
 
+variable "ssh_vault_token" {}
+
 variable "vault_token" {}
 
 variable "vault_addr" {}
@@ -16,12 +18,28 @@ variable "machine_role" {}
 
 //--------------------------------------------------------------------
 // Modules
+
+provider "vault" {
+  token   = "${var.vault_token}"
+  address = "${var.vault_addr}"
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = "${vault_aws_secret_backend.aws.path}"
+  role    = "${vault_aws_secret_backend_role.role.name}"
+}
+
+provider "aws" {
+  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
+  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
+}
+
 data "template_file" "init" {
   template = "${file("${path.root}/userdata.sh")}"
 
   vars {
     vault_addr   = "${var.vault_addr}"
-    vault_token  = "${var.vault_token}"
+    vault_token  = "${var.ssh_vault_token}"
     machine_role = "${var.machine_role}"
   }
 }

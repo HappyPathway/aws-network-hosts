d863c9a9825e09c32337b67038c021f8b52d9ed8
Dave Arnold <darnold@hashicorp.com>
2018-06-30 11:29:54 -0700

    bump

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "template_file" "init" {
 
 module "network_host" {
   source        = "app.terraform.io/Darnold-Hashicorp/network-host/aws"
-  version       = "1.4.0"
+  version       = "1.5.0"
   user_data     = "${data.template_file.init.rendered}"
   count         = "${var.count}"
   network_ws    = "DemoNetwork-East"
